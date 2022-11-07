6c494aedfdbe54193b591c092b9671dc85510dbc
Jonathan-Wills <wills830@hotmail.com>
2022-11-06 20:36:23 -0500

    initial commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,17 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("blow", false)]
+        public void MyTest(string word, bool expected)
         {
-
+            //arrange
+            var test = new WordSmith();
+            //act
+            bool actual = test.IsAPalidrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
